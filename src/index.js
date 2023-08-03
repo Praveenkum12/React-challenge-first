399bba5ea12fc47e28d16da9da5127e88c95b69d
Praveenkum12 <praveenkum1225@gmail.com>
2023-08-03 11:14:38 +0530

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,118 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const skills = [
+  {
+    skill: 'HTML+CSS',
+    level: 'advanced',
+    color: '#2662EA',
+  },
+  {
+    skill: 'JavaScript',
+    level: 'advanced',
+    color: '#EFD81D',
+  },
+  {
+    skill: 'Web Design',
+    level: 'advanced',
+    color: '#C3DCAF',
+  },
+  {
+    skill: 'Git and GitHub',
+    level: 'intermediate',
+    color: '#E84F33',
+  },
+  {
+    skill: 'React',
+    level: 'advanced',
+    color: '#60DAFB',
+  },
+  {
+    skill: 'Svelte',
+    level: 'beginner',
+    color: '#FF3B00',
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="img/christian-burner.jpg"
+      alt="christian-burner"
+    />
+  );
+}
+
+function Intro() {
+  return (
+    <>
+      <h1>Jonas Schedmann</h1>
+      <div>
+        Full-stack web developer and teacher at Udemy. When not coding and
+        preparing a course, I like to play board games, to cook (and eat), or
+        just enjoy the Portugese sun at the beach.
+      </div>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map(skillObj => (
+        <Skill skillObj={skillObj} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skillObj }) {
+  return (
+    <div className="skill" style={{ backgroundColor: skillObj.color }}>
+      <span>{skillObj.skill} </span>
+      {/* <Emoji level={skillObj.level} /> */}
+      <span>
+        {skillObj.level === 'beginner' && '👶'}
+        {skillObj.level === 'intermediate' && '👍'}
+        {skillObj.level === 'advanced' && '💪'}
+      </span>
+    </div>
+  );
+}
+
+// function Emoji({ level }) {
+//   let emoji;
+//   if (level === 'beginner') {
+//     emoji = '👶';
+//   } else if (level === 'intermediate') {
+//     emoji = '👍';
+//   } else {
+//     emoji = '💪';
+//   }
+//   return <span>{emoji}</span>;
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
